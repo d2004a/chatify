c9c64c8ab208f24e7ebb16c44154831910cea8dd
Divyanshu agarwal <agarwaldivyanshu824@gmail.com>
2026-03-10 23:41:44 +0530

    toast updated

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -26,7 +26,7 @@ export const useAuthStore = create((set, get) => ({
             set({ authUser: res.data })
             get().connectSocket()
         } catch (error) {
-            console.log("Error in auth check", error)
+            console.log("Error in auth check",)
             set({ authUser: null })
         } finally {
             set({ isCheckingAuth: false })
