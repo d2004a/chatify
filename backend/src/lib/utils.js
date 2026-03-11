a996025697f03c009c0256e93a84bb28e1c022f3
Divyanshu agarwal <agarwaldivyanshu824@gmail.com>
2026-03-11 18:46:47 +0530

    cores issue solved

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -10,8 +10,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    secure: process.env.NODE_ENV === "development" ? false : true,
+    sameSite: "none",
+    secure: true,
   });
 
   return token;
