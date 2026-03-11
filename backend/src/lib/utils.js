8232d283ba02a82e5232ee06911ae55dc4b256be
Divyanshu agarwal <agarwaldivyanshu824@gmail.com>
2026-03-11 18:27:51 +0530

    cookie error resolved

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -11,7 +11,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === "development" ? false : true,
   });
 
   return token;
